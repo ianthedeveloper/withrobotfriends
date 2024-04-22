c4c12bc4b359f92bc75137344e8c158502bc95e7
mungaii <theforexer1@gmail.com>
2024-04-22 12:42:22 +0300

    Modified: robot.js file

--- src/robots.js
+++ src/robots.js
@@ -1,65 +1,61 @@
 export const robots = [
     {
         id: 1,
-        name: 'Iamine',
-        email: 'iamine@gmail.com'
+        name: "Enterprise",
+        email: "joshoington@gmail.com"
     },
 
     {
-        id: 1,
-        name: 'Iamine',
-        email: 'iamine@gmail.com'
+        id: 2,
+        name: "Kimg Kong",
+        email: "gorillegorille@gmail.com"
     },
 
     {
-        id: 1,
-        name: 'Iamine',
-        email: 'iamine@gmail.com'
+        id: 3,
+        name: "Kim-jun Sen",
+        email: "hehehe@gmail.com"
     },
 
     {
-        id: 1,
-        name: 'Iamine',
-        email: 'iamine@gmail.com'
+        id: 4,
+        name: "Kamata",
+        email: "kiuno@gmail.com"
     },
 
     {
-        id: 1,
-        name: 'Iamine',
-        email: 'iamine@gmail.com'
+        id: 5,
+        name: "Enterpreneur",
+        email: "kingi@gmail.com"
     },
 
     {
-        id: 1,
-        name: 'Iamine',
-        email: 'iamine@gmail.com'
+        id: 6,
+        name: "Owner",
+        email: "laughter@yahoo.com"
     },
 
     {
-        id: 1,
-        name: 'Iamine',
-        email: 'iamine@gmail.com'
+        id: 7,
+        name: "Mjukuu",
+        email: "elenaishn@co.ke"
     },
-    
+
     {
-        id: 1,
-        name: 'Iamine',
-        email: 'iamine@gmail.com'
+        id: 8,
+        name: "Top G",
+        email: "t8@gmail.com"
     },
+
     {
-        id: 1,
-        name: 'Iamine',
-        email: 'iamine@gmail.com'
+        id: 9,
+        name: "Bottom G",
+        email: "kinuthian@gmail.com"
     },
+
     {
-        id: 1,
-        name: 'Iamine',
-        email: 'iamine@gmail.com'
-    },
-    {
-        id: 1,
-        name: 'Iamine',
-        email: 'iamine@gmail.com'
-    },
-    
-]
+        id: 10,
+        name: "GTG",
+        email: "brothers@hotmail.com"
+    }
+];

3e15105b5234dbe8d637ca71d8833bfb8c3b70fc
mungaii <theforexer1@gmail.com>
2024-04-22 13:31:11 +0300

    Activated robohash API

--- src/Card.js
+++ src/Card.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Card = (props) => {
     const {id, name, email} = props;
     return (
-        <div className='bg-light-blue dib br4 ma3 pa3 grow tc'>
-            <img src='https://robohash.org/hihihihiha?212*212' alt='robot img'/>
+        <div className='bg-light-blue dib br4 ma3 pa3 grow tc bw3 shadow-7'>
+            <img src={`https://robohash.org/${id}?212*212`} alt='robot img'/>
             <div>
-                <p>{id}</p>
-                <h2>{name}</h2>
+                <p>{name}</p>
+                <h2>{email}</h2>
             </div>
         </div>
     )

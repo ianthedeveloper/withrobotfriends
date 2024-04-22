1d6c4026fd7ea4a3a652bc4166a898da09ffab2d
mungaii <theforexer1@gmail.com>
2024-04-22 16:15:53 +0300

    Adding CardList Component

---
+++ src/CardList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './Card';
+import {robots} from './robots';
+
+const CardList = () => {
+    const loopingRobots = robots.map((element, i) => {
+        <Card id = {robots[i].id}  name = {robots[i].name}  email = {robots[i].email} /> 
+    });
+
+    return(
+        <div>
+            {loopingRobots}
+        </div>
+    )
+}
+
+export default CardList;

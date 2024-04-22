c326156ac4a7fadcad27b4da6a7856e23302c851
mungaii <theforexer1@gmail.com>
2024-04-22 13:20:11 +0300

    Adding styles: Card component

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
     const {id, name, email} = props;
     return (
-        <div className='tc'>
+        <div className='bg-light-blue dib br4 ma3 pa3 grow tc'>
             <img src='https://robohash.org/hihihihiha?212*212' alt='robot img'/>
             <div>
                 <p>{id}</p>

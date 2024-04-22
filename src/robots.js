04a5f5bda0a02fae41ee1e1993f89d812aff14df
mungaii <theforexer1@gmail.com>
2024-04-22 12:38:51 +0300

    Added Card Component & robot.js file

---
+++ src/robots.js
@@ -0,0 +1,65 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Iamine',
+        email: 'iamine@gmail.com'
+    },
+
+    {
+        id: 1,
+        name: 'Iamine',
+        email: 'iamine@gmail.com'
+    },
+
+    {
+        id: 1,
+        name: 'Iamine',
+        email: 'iamine@gmail.com'
+    },
+
+    {
+        id: 1,
+        name: 'Iamine',
+        email: 'iamine@gmail.com'
+    },
+
+    {
+        id: 1,
+        name: 'Iamine',
+        email: 'iamine@gmail.com'
+    },
+
+    {
+        id: 1,
+        name: 'Iamine',
+        email: 'iamine@gmail.com'
+    },
+
+    {
+        id: 1,
+        name: 'Iamine',
+        email: 'iamine@gmail.com'
+    },
+    
+    {
+        id: 1,
+        name: 'Iamine',
+        email: 'iamine@gmail.com'
+    },
+    {
+        id: 1,
+        name: 'Iamine',
+        email: 'iamine@gmail.com'
+    },
+    {
+        id: 1,
+        name: 'Iamine',
+        email: 'iamine@gmail.com'
+    },
+    {
+        id: 1,
+        name: 'Iamine',
+        email: 'iamine@gmail.com'
+    },
+    
+]

4974d46a495265477008820b17a6240606a5c4ad
mungaii <theforexer1@gmail.com>
2024-04-22 12:53:35 +0300

    Added 1st Card Component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className='tc'>
+            <img src='https://robohash.org/hihihihiha?212*212' alt='robot img'/>
+            <div>
+                <p>1</p>
+                <h2>NaME!</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+import {robots} from './robots';
+import 'tachyons';
 
 ReactDOM.render(
   <Card />,
   document.getElementById('root')
 );
+

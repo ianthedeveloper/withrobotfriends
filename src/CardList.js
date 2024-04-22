274cb5a7d1ba3571721743259f7f289f8bac6c1d
mungaii <theforexer1@gmail.com>
2024-04-22 16:28:28 +0300

    Modifications: CardList Component

--- src/CardList.js
+++ src/CardList.js
@@ -3,9 +3,9 @@ import Card from './Card';
 import {robots} from './robots';
 
 const CardList = () => {
-    const loopingRobots = robots.map((element, i) => {
+    const loopingRobots = robots.map((element, i) => (
         <Card id = {robots[i].id}  name = {robots[i].name}  email = {robots[i].email} /> 
-    });
+    ));
 
     return(
         <div>

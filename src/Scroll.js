3675efa96ce6a9d1288ca492a03ab9d2601622df
mungaii <theforexer1@gmail.com>
2024-04-23 13:08:02 +0300

    Modifications: Scroll Component

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return(
-        <div style = {{overflow: "scroll", border: "2px solid grey", height: "1000px", padding: "11px", margin: "17px"}}  >
+        <div style = {{overflow: "scroll", border: "2px solid grey", height: "1000px", padding: "11px", marginTop: "17px"}}  >
             {props.children}
         </div>
     )

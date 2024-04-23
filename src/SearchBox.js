00ae3ca64cdbc67353bf22f8b4740bf2ebab6eb8
mungaii <theforexer1@gmail.com>
2024-04-23 11:48:39 +0300

    Styling: <h1><h1/, App Component>

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({searchChange}) => {
     return(
         <div>
             <input 
-            className='pa2 ba ba--orange bg-light-red tc'  
+            className='pa2 ba ba--orange bg-white tc'  
             type="search" 
             placeholder="search robot"
             onChange={searchChange} 
